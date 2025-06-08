f16760e697d8a791ac2815d8fc5ce8fa92589368
Sambor <logmykeynoob@gmail.com>
2025-06-08 22:11:08 +0200

    added forward declaration
    added forward declaration for GetTeb function

--- core/utils.h
+++ core/utils.h
@@ -1,12 +1,14 @@
 #pragma once
 #include "ntdefs.hpp"
 #include "peb.hpp"
+#include "teb.hpp"
 
 namespace own::utils {
 
 
 	bool wstrcmp(WPCSTR first, WPCSTR second); // Case sensitive wchar_t array compare
 	_PEB* GetPeb();
+	_TEB* GetTeb();
 
 
 
