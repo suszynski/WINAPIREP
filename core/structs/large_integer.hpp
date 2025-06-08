9f5d5c8756f2f0c45b8456fbdf4c07f1b7da67fd
Sambor <logmykeynoob@gmail.com>
2025-06-08 21:29:26 +0200

    Add files via upload

---
+++ core/structs/large_integer.hpp
@@ -0,0 +1,20 @@
+
+// x64 23H2
+#include "ntdefs.hpp"
+
+using namespace own::ntdefs;
+
+union _LARGE_INTEGER
+{
+    struct
+    {
+        ULONG LowPart;                                                      //0x0
+        LONG HighPart;                                                      //0x4
+    };
+    struct
+    {
+        ULONG LowPart;                                                      //0x0
+        LONG HighPart;                                                      //0x4
+    } u;                                                                    //0x0
+    LONGLONG QuadPart;                                                      //0x0
+};
