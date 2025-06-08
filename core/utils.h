0eeb4e036a903d614b22af41dc73b11f521ee3ac
Sambor <logmykeynoob@gmail.com>
2025-06-08 21:34:13 +0200

    Update utils.h

--- core/utils.h
+++ core/utils.h
@@ -1,1 +1,14 @@
 #pragma once
+#include "ntdefs.hpp"
+#include "peb.hpp"
+
+namespace own::utils {
+
+
+	bool wstrcmp(WPCSTR first, WPCSTR second); // Case sensitive wchar_t array compare
+	_PEB* GetPeb();
+
+
+
+
+}
