58c2368029573a62cb627829625d0405a3e8277e
Sambor <logmykeynoob@gmail.com>
2025-06-08 21:35:07 +0200

    Update ntdefs.hpp

---
+++ core/ntdefs.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#define IN
+#define OPTIONAL
+#define NULL 0
+
+namespace own::ntdefs {
+
+	using HMODULE = void*;
+	using PCSTR = const char*;
+	using WPCSTR = const wchar_t*;
+	using UCHAR = unsigned char;
+	using CHAR = char;
+	using VOID = void*;
+	using USHORT = unsigned short;
+	using ULONG = unsigned long;
+	using WCHAR = wchar_t;
+	using ULONGLONG = unsigned long long;
+	using LONG = long;
+	using LONGLONG = long long;
+}
